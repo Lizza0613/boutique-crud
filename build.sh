36b9de9d86bd12f32048e163a5a5c8463faeefc7
eccontreras07 <ec_contrerasm@hotmail.com>
2026-05-18 00:33:14 -0600

    Agregando makemigrations al script de build

--- build.sh
+++ build.sh
@@ -5,4 +5,7 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
+# ¡Estas dos líneas juntas crean las tablas que faltan!
+python manage.py makemigrations
 python manage.py migrate
