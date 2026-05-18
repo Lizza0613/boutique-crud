92afd6a84758d7c116fe0031e2f383c194132f34
eccontreras07 <ec_contrerasm@hotmail.com>
2026-05-18 01:59:54 -0600

    Sincronizo nombres de funciones de views con urls usando index e id

--- inventario/urls.py
+++ inventario/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 from inventario import views  
 
 urlpatterns = [
-    # 1. Página principal
+    # 1. Página principal conectada a la función index
     path('', views.index, name='index'),
     
-    # 2. Añadir prenda
+    # 2. Ruta para Añadir prenda
     path('producto/crear/', views.crear_producto, name='crear_producto'),
     
-    # 3. Editar prenda
+    # 3. Ruta para Editar (Usa <int:id> igual que la vista)
     path('producto/editar/<int:id>/', views.editar_producto, name='editar_producto'),
     
-    # 4. Eliminar prenda
+    # 4. Ruta para Eliminar (Usa <int:id> igual que la vista)
     path('producto/eliminar/<int:id>/', views.eliminar_producto, name='eliminar_producto'),
 ]
