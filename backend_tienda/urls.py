aec2a7328775611546884e0895c2afc1de8c6835
eccontreras07 <ec_contrerasm@hotmail.com>
2026-05-18 00:04:50 -0600

    Urls limpias de arranque

--- backend_tienda/urls.py
+++ backend_tienda/urls.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from inventario import views  # Asegúrate de importar tus vistas
+from inventario import views
 
 urlpatterns = [
-    path('admin/', admin.site.get_urls() if hasattr(admin.site, 'get_urls') else admin.site.urls),
-    # Esta línea es la clave: al dejar las comillas vacías '', le dices que sea la página de inicio
+    path('admin/', admin.site.urls),
     path('', views.lista_productos, name='lista_productos'),
-    
-    # Aquí abajo debes tener tus otras rutas del CRUD, por ejemplo:
     path('crear/', views.crear_producto, name='crear_producto'),
     path('editar/<int:id>/', views.editar_producto, name='editar_producto'),
     path('eliminar/<int:id>/', views.eliminar_producto, name='eliminar_producto'),
